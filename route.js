6a0ed002a3d2112be5c3b20845268cd10eaa011d
Barmaganbetov <46858738+Barmaganbetov@users.noreply.github.com>
2019-01-20 16:33:27 +0600

    Update route.js

--- route.js
+++ route.js
@@ -1,23 +1,32 @@
-ymaps.ready(function () {
-var location = ymaps.geolocation;
-var myMap = new ymaps.Map('map', {
-    center: [42.36, 69.49],
-    zoom: 14,
-        // Добавим кнопку для построения маршрутов на карту.
-        controls: ['routeButtonControl', 'geolocationControl', 'zoomControl']
-    });
-    var control = myMap.controls.get('routeButtonControl');
+ymaps.ready(init);
 
-    // Зададим координаты пункта отправления с помощью геолокации.
-    control.routePanel.geolocate('from');
+function init() {
+	var userCoodinates;
+    var geolocation = ymaps.geolocation,
+        myMap = new ymaps.Map('map', {
+            center: [42.316086,69.666779],
+            zoom: 12,
+			controls: ['routeButtonControl','geolocationControl', 'zoomControl']
+        });
+
+    geolocation.get({
+        provider: 'browser',
+        mapStateAutoApply: true
+    }).then(function (result) {
+        // Синим цветом пометим положение, полученное через браузер.
+        // Если браузер не поддерживает эту функциональность, метка не будет добавлена на карту.        
+		userCoodinates = result.geoObjects.get(0).geometry.getCoordinates();
+	    var control = myMap.controls.get('routeButtonControl');
 
     // Откроем панель для построения маршрутов.
     control.state.set('expanded', true);
-	
-	control.routePanel.geolocate('from');
 	control.routePanel.state.set({
+	fromEnabled: false,
+    from: userCoodinates,
     to: [42.36476, 69.494534],
     type: "auto"
 });
-	
+		
 });
+
+}
